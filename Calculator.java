e6edb5e80dfc8e25f11a28052abf63818d1bbe91
Fix1t <gbiel1117@gmail.com>
2023-06-04 10:55:40 +0200

    structure

--- Calculator.java
+++ Calculator.java
@@ -1,3 +1,4 @@
+package com.example.calculator;
 public class Calculator {
     public int add(int a, int b) {
         return a + b;
--- tests/CalculatorTest.java
+++ tests/CalculatorTest.java
@@ -1,54 +1,50 @@
-import org.junit.*;
-import static org.junit.Assert.*;
+package com.example.calculator;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
+
     private Calculator calculator;
 
-    @Before
+    @BeforeEach
     public void setUp() {
         calculator = new Calculator();
     }
 
     @Test
     public void testAdd() {
-        int a = 3;
-        int b = 4;
-        int expected = 7;
-        int result = calculator.add(a, b);
-        assertEquals(expected, result);
+        int a = 10;
+        int b = 20;
+        int expected = 30;
+        int actual = calculator.add(a, b);
+        assertEquals(expected, actual);
     }
 
     @Test
     public void testSubtract() {
-        int a = 6;
-        int b = 4;
-        int expected = 2;
-        int result = calculator.subtract(a, b);
-        assertEquals(expected, result);
+        int a = 20;
+        int b = 10;
+        int expected = 10;
+        int actual = calculator.subtract(a, b);
+        assertEquals(expected, actual);
     }
 
     @Test
     public void testMultiply() {
-        int a = 5;
-        int b = 4;
-        int expected = 20;
-        int result = calculator.multiply(a, b);
-        assertEquals(expected, result);
+        int a = 10;
+        int b = 20;
+        int expected = 200;
+        int actual = calculator.multiply(a, b);
+        assertEquals(expected, actual);
     }
 
     @Test
     public void testDivide() {
-        int a = 10;
-        int b = 2;
-        int expected = 5;
-        int result = calculator.divide(a, b);
-        assertEquals(expected, result);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testDivideByZero() {
-        int a = 10;
-        int b = 0;
-        calculator.divide(a, b);
+        int a = 20;
+        int b = 10;
+        int expected = 2;
+        int actual = calculator.divide(a, b);
+        assertEquals(expected, actual);
     }
 }
